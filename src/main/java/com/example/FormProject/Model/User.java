230a23f976b8176c734e89dcb5aafa9989b87981
Shaik Fayaz <fayaz@Shaiks-MacBook-Air.local>
2024-09-17 19:14:22 +0530

    in user class userName changed to username

--- src/main/java/com/example/FormProject/Model/User.java
+++ src/main/java/com/example/FormProject/Model/User.java
@@ -11,7 +11,7 @@ public class User {
 
     @Id
     private String id;
-    private String userName;
+    private String username;
     private String password;
     private Long phoneNumber;
     
